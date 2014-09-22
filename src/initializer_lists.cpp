752da1483cb8ac0e7817dc4a371ae841e36feb96
Nicolas Léveillé <nicolas@uucidl.com>
2014-09-22 10:07:55 +0200

    test on Visual Studio 2013 Express Update 3
    On this one we got a runtime crash (when initializer lists are not
    tagged with the target type) and a compiler crash with a more complex
    aggregate.

--- src/initializer_lists.cpp
+++ src/initializer_lists.cpp
@@ -1,6 +1,14 @@
 #include <vector>
 #include <string>
 
+static bool skip_crashes = false;
+#define SKIP_COMPILER_CRASHES 0
+
+#define BEGIN_CRASHES_ON(desc) if (!skip_crashes) {
+#define END_CRASHES_ON }
+#define BEGIN_COMPILER_CRASH_ON(desc) {
+#define END_COMPILER_CRASH_ON }
+
 struct A
 {
         std::string name;
@@ -32,12 +40,35 @@ size_t extractor2(A value, B collection)
 
 void test_initializer_lists()
 {
-        auto value = extractor({ "Hello, World" });
-        printf("should be a greeting: %s\n", value.c_str());
+        BEGIN_CRASHES_ON("Visual Studio Express 2013 Update 3")
+        {
+                auto value = extractor({ "Hello, World" });
+                printf("should be a greeting: %s\n", value.c_str());
+        }
+        END_CRASHES_ON;
 
-        auto index = extractor2(
-            { "b" }, { "collection", { { "a" }, { "b" }, { "c" } } });
-        printf("should be 1: %lu\n", index);
+        {
+                auto value = extractor(A{ "Hello, World" });
+                printf("should be a greeting: %s\n", value.c_str());
+        }
+
+        BEGIN_CRASHES_ON("Visual Studio Express 2013 Update 3")
+        {
+                auto index = extractor2(
+                    { "b" }, { "collection", { { "a" }, { "b" }, { "c" } } });
+                printf("should be 1: %lu\n", index);
+        }
+        END_CRASHES_ON;
+
+#if !SKIP_COMPILER_CRASHES
+        BEGIN_COMPILER_CRASH_ON("Visual Studio Express 2013 Update 3")
+        {
+                auto index = extractor2(
+                    A{ "b" }, { "collection", { { "a" }, { "b" }, { "c" } } });
+                printf("should be 1: %lu\n", index);
+        }
+        END_COMPILER_CRASH_ON;
+#endif
 }
 
 int main()
